4ab006431385c26c99325c253d440977b8a922a3
alanvieyra333 <arinconf@globalhitss.com>
2019-01-23 19:29:29 -0600

    Se subio la version de io.spring.platform = Athens-RELEASE

--- src/main/java/com/telmex/application/IndexController.java
+++ src/main/java/com/telmex/application/IndexController.java
@@ -1,19 +1,17 @@
 package com.telmex.application;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 	
-	@RequestMapping(value = "/",
-			method = RequestMethod.GET,
+	@GetMapping(value = "/",
 			produces = {MediaType.APPLICATION_XML_VALUE})
 	public Object index() {
 		Ex example = new Ex();
-		example.setOk("Hello!");
+		example.setOk("Hello world!");
 		return example;
 	}
 	
